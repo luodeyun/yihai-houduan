9c30d9b1ae863b93133420d5bffced46b98f8bc2
ldy <807799686@qq.com>
2020-12-10 21:24:27 +0800

    session

--- router/login.js
+++ router/login.js
@@ -3,16 +3,17 @@ var svgCaptcha = require ('svg-captcha')
 let router = new Router()
 router.get('/captcha',async(req,res)=>{
     var captcha = svgCaptcha.create({noise:3,background: '#aa9986',color:true});
-    req.session.captcha = captcha.text
+    console.log(req.session);
+    req.session.captcha = captcha.text.toLowerCase()
     res.type('svg');
     console.log("captcha:",req.session)
 	res.status(200).send(captcha.data);
 })
 router.post('/login',async(req,res)=>{
     const {phone,password,code} = req.body
-     console.log(code);
-     if(code==req.session.captcha)
+     console.log(req.session);   
+     if(code.toLowerCase()==req.session.captcha)
      {res.send('登录成功')} 
-     else{res.send('验证码不正确(注意大小写)')}
+     else{res.send('验证码不正确')}
 })
 module.exports = router
