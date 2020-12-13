d9ab34bda04fbd68e14b7df68591541bd7162ddd
ldy <807799686@qq.com>
2020-12-13 22:04:55 +0800

    yihai 07

--- router/discoverRouter.js
+++ router/discoverRouter.js
@@ -2,12 +2,22 @@ let discoverModel = require('../model/discoverModel')
 
 let {Router} = require('express')
 let router = new Router()
-router.get('/discover', async (req, res) => {
+router.get('/discover', async (req, res) => { //查询集体数据
     try {
       let result = await discoverModel.find()
       res.send(result)
     }catch (err) {
         res.send('网络异常')
     }
+})                              
+router.get('/cover', async(req,res)=>{
+  try{
+      let operationId = req.query.operationId     
+      console.log(operationId);      
+      let resdata = await discoverModel.findOne({operationId})
+      res.send(resdata.result)
+  }catch(err){
+      res.send('网络异常')
+  }   
 })
 module.exports = router
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -10,12 +10,12 @@ router.get('/captcha',async(req,res)=>{
     //  上面操作一句代码搞定  req.session 
     req.session.captcha = captcha.text.toLowerCase()
     res.type('svg');
-    console.log("captcha:",req.session)
+    
 	res.status(200).send(captcha.data);
 })
 router.post('/login',async(req,res)=>{
     const {phone,password,code} = req.body
-     console.log(req.session);   
+       
      if(code.toLowerCase()==req.session.captcha) //且数据库有此信息则登录成功
      {res.send('登录成功')} 
      else {res.send('验证码不正确')}
